df19799eb74a552ce812957c2ee80f577161f916
jojohnathon <pixelizedjohnny@gmail.com>
2023-02-07 17:10:16 -0800

    add wrist gear ratio constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,6 +63,7 @@ public class Constants {
     public static class WristConstants {
         public static final int intakeMotor = 97;
         public static final int wristMotor = 98;
+        public static final int gearRatio = 64;
 
         public static final double kMaxVelocity = 0;
         public static final double kMaxAcceleration = 0;
